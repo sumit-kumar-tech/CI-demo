30f731e35d1cf05164793c7c4723d0d8fdb4cf1a
sumit-kumar-tech <sumitme752@gmail.com>
2026-07-01 15:39:41 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-def add(a,b):
+def add(a, b):
     return a+b
 
 
-
-
-
-def sub(a,b):
+def sub(a, b):
     return a-b
 
-r1 = add(4,5)
-r2 = sub(4,2)
 
-print(r1,r2)
+r1 = add(4, 5)
+r2 = sub(4, 2)
+
+print(r1, r2)
